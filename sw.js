a1e9e8ee1a740597c87dccb293664608af501d89
mjahedvai2-lang <mjahedvai2@gmail.com>
2026-09-12 21:41:40 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,30 @@
-const CACHE_NAME = 'gaming-network-helper-v1';
+const CACHE_NAME = "gaming-network-helper-v1";
 
-const APP_SHELL = [
-  './',
-  './index.html'
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512-1.png"
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(APP_SHELL);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(APP_FILES);
     })
   );
 
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then((keys) => {
+    caches.keys().then(keys => {
       return Promise.all(
         keys
-          .filter((key) => key !== CACHE_NAME)
-          .map((key) => caches.delete(key))
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
       );
     })
   );
@@ -29,28 +32,37 @@ self.addEventListener('activate', (event) => {
   self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.method !== 'GET') return;
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") {
+    return;
+  }
 
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
+    caches.match(event.request).then(cachedResponse => {
+
       if (cachedResponse) {
         return cachedResponse;
       }
 
       return fetch(event.request)
-        .then((response) => {
+        .then(response => {
+
+          if (!response || response.status !== 200) {
+            return response;
+          }
+
           const responseClone = response.clone();
 
-          caches.open(CACHE_NAME).then((cache) => {
+          caches.open(CACHE_NAME).then(cache => {
             cache.put(event.request, responseClone);
           });
 
           return response;
         })
         .catch(() => {
-          return caches.match('./index.html');
+          return caches.match("./index.html");
         });
+
     })
   );
 });
